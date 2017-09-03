2886e0baf2a3e7bbcaf5fdf4ab24d9d4cdb2108d
Paul Tan <pyokagan@gmail.com>
2017-09-04 00:09:16 +0800

    arduino: main.c -> main.cpp
    Compiling as a C++ is needed so that `Serial.begin()`,
    `Serial.println()` will work.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,5 @@
-// Dummy file for the Arduino IDE, which requires *.ino instead of *.c.
+// Dummy file for the Arduino IDE, which requires *.ino instead of *.cpp.
 //
 // DO NOT ADD ANY CODE TO THIS FILE!!!
 //
-// Add it to main.c (or any of the other files) instead!
+// Add it to main.cpp (or any of the other files) instead!
