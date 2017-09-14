203520bae3b053912cd8a523fd0bd5d2500d1572
Paul Tan <pyokagan@gmail.com>
2017-09-14 22:13:37 +0800

    ucomm: Add size assertions for message structs
    This allows us to detect if the compiler sneakily adds any padding.

--- libraries/ucomm/src/ucomm.h
+++ libraries/ucomm/src/ucomm.h
@@ -3,9 +3,17 @@
 #include "userial.h"
 #include "uframe.h"
 #include <stdint.h>
+#include <assert.h>
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#define ASSERT_STRINGIFY(x) #x
+#if (__cplusplus >= 201103L) || defined(static_assert)
+#define ASSERT_SIZE(st, s) static_assert(sizeof(st) == s, ASSERT_STRINGIFY(st) " does not have size " ASSERT_STRINGIFY(s))
+#else
+#define ASSERT_SIZE(st, s) typedef char static_assert_ ## st ## _does_not_have_size_ ## s [(!!(sizeof(st) == s)) * 2 - 1]
 #endif
 
 typedef enum {
@@ -16,6 +24,7 @@ typedef struct ucomm_MessageHeader {
     uint8_t type;
     uint8_t id;
 } ucomm_MessageHeader;
+ASSERT_SIZE(ucomm_MessageHeader, 2);
 
 typedef struct ucomm_MessageSample {
     uint8_t type; // UCOMM_MESSAGE_SAMPLE
@@ -28,6 +37,7 @@ typedef struct ucomm_MessageSample {
     int16_t acc2z;
     uint16_t __dummy_crc16;
 } ucomm_MessageSample;
+ASSERT_SIZE(ucomm_MessageSample, 16);
 
 typedef union ucomm_Message {
     ucomm_MessageHeader header;
@@ -39,6 +49,9 @@ void ucomm_init(void);
 void ucomm_read(ucomm_Message *);
 
 void ucomm_write(const ucomm_Message *);
+
+#undef ASSERT_SIZE
+#undef ASSERT_STRINGIFY
 
 #ifdef __cplusplus
 }
