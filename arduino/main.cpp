e77de9010f4e846a70fb470d01f6f77bf34cd192
My Duy Hoang Long <myduyhoanglong@gmail.com>
2017-09-20 23:21:43 +0800

    debug

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -12,16 +12,14 @@ void uart1_ISR() {
 
 void setup() {
     Serial.begin(115200); // serial for debug messages
+	attachInterrupt(0, uart1_ISR, RISING);
     taskComm_setup();
     taskI2C_setup();
-	attachInterrupt(0, uart1_ISR, RISING);
-}
-
-void loop() {
     xTaskCreate(taskComm, "Comm", 128, NULL, 1, NULL);
     xTaskCreate(taskRecv, "Recv", 128, NULL, 1, NULL);
     xTaskCreate(taskPow, "Pow", 128, NULL, 2, NULL);
     xTaskCreate(taskI2C, "I2C", 128, NULL, 3, NULL);
-	
-	vTaskStartScheduler();
+}
+
+void loop() {
 }
