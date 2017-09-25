1aa4f7861544b8b47af8295491a26157bfe182fd
My Duy Hoang Long <myduyhoanglong@gmail.com>
2017-09-25 19:28:29 +0800

    Rewrite uart1

--- arduino/uart1.cpp
+++ arduino/uart1.cpp
@@ -1,22 +1,62 @@
 #include "uart1.h"
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <HardwareSerial.h>
+#include <HardwareSerial_private.h>
+#include <wiring_private.h>
+
+static HardwareSerial UART1(&UBRR1H, &UBRR1L, &UCSR1A, &UCSR1B, &UCSR1C, &UDR1);
+static SemaphoreHandle_t lock_read;
+
+#if defined(UART1_RX_vect)
+ISR(UART1_RX_vect)
+#elif defined(USART1_RX_vect)
+ISR(USART1_RX_vect)
+#else
+#error "Don't know what the Data Register Empty vector is called for Serial1"
+#endif
+{
+    static BaseType_t yieldWhenComplete;
+    yieldWhenComplete = pdFALSE;
+
+    UART1._rx_complete_irq();
+    xSemaphoreGiveFromISR(lock_read, &yieldWhenComplete);
+    if (yieldWhenComplete)
+        taskYIELD();
+}
+
+#if defined(UART1_UDRE_vect)
+ISR(UART1_UDRE_vect)
+#elif defined(USART1_UDRE_vect)
+ISR(USART1_UDRE_vect)
+#else
+#error "Don't know what the Data Register Empty vector is called for Serial1"
+#endif
+{
+    UART1._tx_udr_empty_irq();
+}
 
 void
 uart1_init(void)
 {
-    Serial1.begin(115200);
+    lock_read = xSemaphoreCreateBinary();
+    UART1.begin(115200);
 }
 
 void
 uart1_write(uint8_t byte)
 {
-    Serial1.write(byte);
+    UART1.write(byte);
 }
 
 uint8_t
 uart1_read(void)
 {
-    while (Serial1.available() <= 0);
-    return Serial1.read();
+    while (UART1.available() <= 0) {
+        while (xSemaphoreTake(lock_read, portMAX_DELAY) != pdTRUE);
+    }
+    return UART1.read();
 }
