ee7ed25ab5cfb5f7d1d7ef011f6767d93ecbaaac
Paul Tan <pyokagan@gmail.com>
2017-09-25 19:16:49 +0800

    arduino: increase taskComm/taskRecv stack size
    These tasks use up quite a bit of stack space (probably to store all the
    ucomm_Message structs).
    
    To reduce the probability of them causing a stack overflow, increase
    their stack size.

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -7,8 +7,8 @@ void setup() {
     Serial.begin(115200); // serial for debug messages
     taskComm_setup();
     taskI2C_setup();
-    xTaskCreate(taskComm, "Comm", 128, NULL, 1, NULL);
-    xTaskCreate(taskRecv, "Recv", 128, NULL, 1, NULL);
+    xTaskCreate(taskComm, "Comm", 256, NULL, 1, NULL);
+    xTaskCreate(taskRecv, "Recv", 256, NULL, 1, NULL);
     xTaskCreate(taskI2C, "I2C", 128, NULL, 2, NULL);
     xTaskCreate(taskPow, "Pow", 128, NULL, 3, NULL);
 }
