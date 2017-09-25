db3b21dc6d5a2e3f84a7653eb0dbe521a9a17612
Paul Tan <pyokagan@gmail.com>
2017-09-25 19:21:38 +0800

    arduino: make taskPow priority > taskI2C priority
    Given that taskPow runs at a much lower frequency compared to taskI2C,
    it should have a higher priority to ensure that it actually gets a
    chance to run.

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -9,8 +9,8 @@ void setup() {
     taskI2C_setup();
     xTaskCreate(taskComm, "Comm", 128, NULL, 1, NULL);
     xTaskCreate(taskRecv, "Recv", 128, NULL, 1, NULL);
-    xTaskCreate(taskPow, "Pow", 128, NULL, 2, NULL);
-    xTaskCreate(taskI2C, "I2C", 128, NULL, 3, NULL);
+    xTaskCreate(taskI2C, "I2C", 128, NULL, 2, NULL);
+    xTaskCreate(taskPow, "Pow", 128, NULL, 3, NULL);
 }
 
 void loop() {
